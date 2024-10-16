aba676295c944fd9a18457dc5cf41562b194ff7c
Jenish <jpat534@gmail.com>
2024-10-16 16:00:34 +1100

    Add a function to create a Store table.

--- models/Store.ts
+++ models/Store.ts
@@ -1,5 +1,18 @@
-
+import { openDataBase } from "@/lib/db";
 
 export const setUpStoresTable = async () => {
+    try {
+        const db = await openDataBase(); // Open the database asynchronously
+        await db.execAsync(`
+            CREATE TABLE IF NOT EXISTS stores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT
+            name TEXT NOT NULL
+            )
+            
+            `);
+            console.log('Stores table created successfully');
+    } catch (error) {
+        console.error('Error creating stores tables:', error);
 
+    }
 }
