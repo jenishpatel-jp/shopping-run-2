01e1b8f641168b8c649468195a784220faf57e27
Jenish <jpat534@gmail.com>
2024-10-16 16:44:18 +1100

    updated the items table function.
    It now correctly creates a Foreign Key and Boolean.

--- models/Items.ts
+++ models/Items.ts
@@ -4,12 +4,14 @@ export const setUpItemsTable = async () => {
     try {
         const db = await openDataBase(); // Open the database
         await db.execAsync(`
-            CREATE TABLE IF NOT EXIST items (
+            PRAGMA journal_mode = WAL;
+            CREATE TABLE IF NOT EXISTS items (
             itemId INTEGER PRIMARY KEY AUTOINCREMENT,
             storeId INTEGER NOT NULL,
             itemName TEXT NOT NULL,
-            completed BOOLEAN
-            )
+            completed INTEGER DEFAULT 0, -- Use INTEGER for boolean values (0 = false, 1 = true)
+            FOREIGN KEY (storeId) REFERENCES stores (id) ON DELETE CASCADE
+            );
             `);
         console.log('Items table created successfully');
     } catch (error) {
--- models/Store.ts
+++ models/Store.ts
@@ -4,11 +4,11 @@ export const setUpStoresTable = async () => {
     try {
         const db = await openDataBase(); // Open the database asynchronously
         await db.execAsync(`
+            PRAGMA journal_mode = WAL;
             CREATE TABLE IF NOT EXISTS stores (
             storeId INTEGER PRIMARY KEY AUTOINCREMENT,
             storeName TEXT NOT NULL
             )
-            
             `);
         console.log('Stores table created successfully');
     } catch (error) {
