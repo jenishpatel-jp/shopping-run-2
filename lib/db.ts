62961a73478b77419101e21bbc1b82349493b581
Jenish <jpat534@gmail.com>
2024-10-16 15:54:49 +1100

    Updated the code for the database so it uses an async function

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,14 @@
 import * as SQLite from 'expo-sqlite';
 
-const db = SQLite.openDatabaseAsync('shopping_list.db');
+// Async function to open or create the database.
+
+export const openDataBase = async () => {
+    try {
+        const db = await SQLite.openDatabaseAsync('shopping_run.db');
+        console.log('Database opened successfully');
+        return db;
+    } catch (error){
+        console.error('Error opening database:', error);
+        throw error;
+    }
+}
---
+++ models/Store.ts
@@ -0,0 +1,5 @@
+
+
+export const setUpStoresTable = async () => {
+
+}
