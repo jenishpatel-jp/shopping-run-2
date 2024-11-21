9342f2f8bf074f2194825386f1752a20bed50be7
Jenish <jpat534@gmail.com>
2024-11-21 20:18:31 +1100

    Added the Add button

--- components/Items/Items.tsx
+++ components/Items/Items.tsx
@@ -117,96 +117,15 @@ const Items: React.FC<ItemsProps> = ( {newStoreName, setNewStoreName} ) => {
                         onPressIn={()=> setButtonPressed(true)}
                         onPressOut={(()=> setButtonPressed(false))}
                     >
-
-                    </Pressable>
                     <Text style={styles.buttonText}>
                         Add
                     </Text>
+                    </Pressable>
                     
                 </View>
              
         
         </View>
-
-
-        
-        // <View style={styles.card} >
-
-        //     {/* Text input to enter each items name */}
-
-        //     {/* mapping the stores */}
-        //     {storeList.map( (store, index) => (
-        //     <View key={index} style={styles.storeContainer} >
-
-        //     {/* If the store is being edited, a text input will appear, otherwise the store name that has been set will appear */}
-
-        //     {editingStoreIndex === index ? (
-        //         <TextInput
-        //             style={styles.editTextInput}
-        //             value={newStoreName}
-        //             onChangeText={setNewStoreName}
-        //             />
-        //         ): (
-        //     <View style={styles.checkboxContainer} >
-        //             <CustomCheckbox 
-        //                 key={index}
-        //                 checked={ selectedStore === store }
-        //                 onPress={ () => selectStoreFunction(store) }
-        //             />
-        //             <Text style={styles.checkboxText} >{store}</Text>
-
-        //     </View>
-        //         )}
-
-        //     {/* If the store is being edited, the update button will appear, otherwise the edit and trash icon will appear */}
-
-        //     <View style={styles.updateView} >
-
-        //         {editingStoreIndex === index ? (
-        //         <Pressable onPress={updateStoreName} >
-        //             <Text style={styles.buttonText} >Update</Text>
-        //         </Pressable>
-
-        //         ):(
-
-        //         <Pressable
-        //             onPress={() => editStore(index)}
-        //         >
-        //             <Feather style={styles.edit} name="edit" size={26} color="#F5A418" />
-        //         </Pressable>
-        //         )}
-
-        //         <Pressable
-        //         onPress={() => deleteStore(index) }
-        //         >
-        //             <MaterialIcons style={styles.delete}  name="delete-outline" size={30} color="#F5A418" />
-        //         </Pressable>
-        //     </View>
-        //     </View>
-        //     )
-        //     )}
-
-        //     {/* Add Button  */}
-        //     <View style={styles.addButtonContainer} >
-        //         <Pressable
-        //             onPress={
-        //             handleAddItem
-        //             }
-        //             onPressIn={() => setButtonPressed(true)}
-        //             onPressOut={() => setButtonPressed(false)}
-        //             style={( {pressed} ) => [
-        //                 {
-        //                     backgroundColor: pressed || buttonPressed ? "#5200A3" : "#40146B"
-        //                 }
-        //             ]}
-        //             >
-        //             <Text style={styles.buttonText}>
-        //                 Add
-        //             </Text>
-        //         </Pressable>
-        //     </View>
-
-        // </View>
     )
 };
 
