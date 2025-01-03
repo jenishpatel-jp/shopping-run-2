ea15909361388b134ed28c4a04181696412c5524
Jenish <jpat534@gmail.com>
2025-01-03 17:37:18 +1100

    Updated the code to kind of work

--- components/Lists/Lists.tsx
+++ components/Lists/Lists.tsx
@@ -30,8 +30,16 @@ interface Section {
 const Lists: React.FC<ListsProps> = ({ stores, items }) => {
   //useState to determine which item has been selected
   const [storeAndItems, setStoreAndItems] = useState<Record<string, string[]>>({});
-  const [selectedItem, setSelectedItem] = useState<string | null>(null);
   const [completedItems, setCompletedItems] = useState<string[]>([]);
+
+  //useState to determine which item has been selected
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+
+  //useState to determine which store and item is being edited
+  const [storeOfItem, setStoreOfItem] = useState<string | null>(null);
+
+  //useState to determine which item is being edited
+  const [indexOfItem, setIndexOfItem] = useState<number | null>(null);
 
   //Need to create a useState that stores the objects {store: [item, item, item]}
 
@@ -70,99 +78,67 @@ const Lists: React.FC<ListsProps> = ({ stores, items }) => {
   // if completedItem has a item in it, a title called 'Completed' is added. The data is the list of completed items.
   if (completedItems.length > 0) {
     sections.push({ title: 'Completed', data: completedItems });
-  }
-
-
+  };
 
   return (
-    <SectionList
-      sections={sections}
-      renderSectionHeader={({ section }) => (
-        <Text style={styles.storeName}>{section.title}</Text>
-      )}
-      renderItem={({ item, section }) => (
-        // Still need to code how a completed item would look like
-
-
-
-
-        <View style={styles.itemsContainer}>
-          <View style={styles.checkboxContainer}>
-            <Checkbox
-              style={styles.checkbox}
-              onValueChange={() => console.log("Checkbox has been checked")}
-            />
-          </View>
-          <View>
-            <Pressable>
-              <MaterialIcons
-                style={styles.delete}
-                name="delete-outline"
-                size={30}
-                color="#F5A418"
-              />
-            </Pressable>
-          </View>
-        </View>
-      )}
-    />
+    
 
     // Uses the SectionList component to render the header and list
-    // <SectionList
-    //     sections={sections}
-    //     renderSectionHeader={({ section }) => (
-    //         <Text style={styles.storeName}>{section.title}</Text>
-    //     )}
-    //     renderItem={({ item, section }) => (
-    //         <View>
-    //             {/* If the title is not completed, it will render the items, otherwise it will render the completed list */}
-    //             {section.title !== 'Completed' ? (
-    //                 <View style={styles.itemsContainer}>
+    <SectionList
+        sections={sections}
+        renderSectionHeader={({ section }) => (
+            <Text style={styles.storeName}>{section.title}</Text>
+        )}
+        renderItem={({ item, section }) => (
+            <View>
+                {/* If the title is not completed, it will render the items, otherwise it will render the completed list */}
+                {section.title !== 'Completed' ? (
+                    <View style={styles.itemsContainer}>
 
-    //                     {/* Checks if the store name and item is the selected item. This view shows shows the text input or checkbox */}
-    //                     {storeOfItem === section.title && indexOfItem === shoppingList[section.title].indexOf(item) ? (
-    //                         <TextInput
-    //                             style={styles.editTextInput}
-    //                             value={newItemName}
-    //                             onChangeText={setNewItemName}
-    //                         />4
-    //                     ) : (
-    //                         <View style={styles.checkboxContainer}>
-    //                             <Checkbox
-    //                                 style={styles.checkbox}
-    //                                 color={selectedItem === item ? "#F5A418" : "#F5A418"}
-    //                                 value={selectedItem === item}
-    //                                 onValueChange={() => {
-    //                                     setSelectedItem(item);
-    //                                     checkOffItem(item);
-    //                                 }}
-    //                             />
-    //                             <Text style={styles.checkboxText}> {item} </Text>
-    //                         </View>
-    //                     )}
-    //                     {/* The view will show Update if the item has been ticked, otherwise it will show the edit icon */}
-    //                     <View style={styles.updateView}>
-    //                         {storeOfItem === section.title && indexOfItem === shoppingList[section.title].indexOf(item) ? (
-    //                             <Pressable onPress={updateItemName}>
-    //                                 <Text style={styles.buttonText}>Update</Text>
-    //                             </Pressable>
-    //                         ) : (
-    //                             <Pressable onPress={() => editItem(section.title, item)}>
-    //                                 <Feather style={styles.edit} name="edit" size={26} color="#F5A418" />
-    //                             </Pressable>
-    //                         )}
-    //                         <Pressable onPress={() => deleteItem(section.title, item)}>
-    //                             <MaterialIcons style={styles.delete} name="delete-outline" size={30} color="#F5A418" />
-    //                         </Pressable>
-    //                     </View>
-    //                 </View>
-    //             ) : (
-    //                 <Text style={styles.completedItems}>{item}</Text>
-    //             )}
-    //         </View>
-    //     )}
-    //     keyExtractor={(item, index) => item + index}
-    // />
+                        {/* Checks if the store name and item is the selected item. This view shows shows the text input or checkbox */}
+                        {storeOfItem === section.title && indexOfItem === storeAndItems[section.title].indexOf(item) ? (
+                            <TextInput
+                                style={styles.editTextInput}
+                                //value={storeAndItems}
+                                onChangeText={()=> console.log('onChangeText')}
+                            />
+                        ) : ( 
+                            <View style={styles.checkboxContainer}>
+                                <Checkbox
+                                    style={styles.checkbox}
+                                    color={selectedItem === item ? "#F5A418" : "#F5A418"}
+                                    value={selectedItem === item}
+                                    onValueChange={() => {
+                                        setSelectedItem(item);
+                                        //checkOffItem(item);
+                                    }}
+                                />
+                                <Text style={styles.checkboxText}> {item} </Text>
+                            </View>
+                        )}
+                        {/* The view will show Update if the item has been ticked, otherwise it will show the edit icon */}
+                        <View style={styles.updateView}>
+                            {storeOfItem === section.title && indexOfItem === storeAndItems[section.title].indexOf(item) ? (
+                                <Pressable onPress={()=> console.log('update')}>
+                                    <Text style={styles.buttonText}>Update</Text>
+                                </Pressable>
+                            ) : (
+                                <Pressable onPress={() => console.log('edit')}>
+                                    <Feather style={styles.edit} name="edit" size={26} color="#F5A418" />
+                                </Pressable>
+                            )}
+                            <Pressable onPress={() => console.log('delete')}>
+                                <MaterialIcons style={styles.delete} name="delete-outline" size={30} color="#F5A418" />
+                            </Pressable>
+                        </View>
+                    </View>
+                ) : (
+                    <Text style={styles.completedItems}>{item}</Text>
+                )}
+            </View>
+        )}
+        keyExtractor={(item, index) => item + index}
+    />
   );
 };
 
